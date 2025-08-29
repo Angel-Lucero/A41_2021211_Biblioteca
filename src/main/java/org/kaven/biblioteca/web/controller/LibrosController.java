4280ba9048cbc7cd1229795140aaab66a931cb93
Angel Lucero <alucero-2021211@kinal.edu.gt>
2025-08-29 14:54:28 -0600

    Parte de libros

--- src/main/java/org/kaven/biblioteca/web/controller/LibrosController.java
+++ src/main/java/org/kaven/biblioteca/web/controller/LibrosController.java
@@ -1,10 +1,13 @@
 package org.kaven.biblioteca.web.controller;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import lombok.Data;
 import org.kaven.biblioteca.dominio.service.ILibroService;
 import org.kaven.biblioteca.persistence.entity.Libro;
+import org.primefaces.PrimeFaces;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,16 +26,48 @@ public class LibrosController {
     private static final Logger logger = LoggerFactory.getLogger(LibrosController.class);
 
     @PostConstruct
-    public void init(){
+    public void init() {
         cargarDatos();
     }
 
-    public void cargarDatos(){
+    public void cargarDatos() {
         this.libros = this.libroService.listarLibros();
         this.libros.forEach(libro -> logger.info(libro.toString()));
     }
 
-    public void agregarCliente(){
+    public void agregarLibros() {
         this.libroSeleccionado = new Libro();
     }
+
+    public void guardarLibros(){
+        logger.info("Libro a guardar" + this.libroSeleccionado);
+
+        if (this.libroSeleccionado.getIdlibro() == null){
+            this.libroService.guardarLibro(this.libroSeleccionado);
+            this.libros.add(this.libroSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("Libro agregado"));
+        }
+        else{
+            this.libroService.guardarLibro(this.libroSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("Libro Actualizado"));
+        }
+        PrimeFaces.current().executeScript("PF('VentanaModalLibro').hide()");
+        PrimeFaces.current().ajax().update("formulario-libros:mensaje_emergente",
+                "formulario-libros:tabla-libros");
+        this.libroSeleccionado = null;
+    }
+
+    public void eliminarLibros(){
+        logger.info("Libro a eliminar: "+this.libroSeleccionado);
+        this.libroService.eliminarLibro(libroSeleccionado);
+        this.libros.remove(libroSeleccionado);
+        this.libroSeleccionado=null;
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Libro eliminado"));
+        PrimeFaces.current().ajax().update("formulario-libros:mensaje_emergente",
+                "formulario-libros:tabla-libros");
+    }
 }
+
+
